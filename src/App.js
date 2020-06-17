202282a6438f22b6eeb7d3844123ab77108cde30
raviteja8k <raviteja.tk@gmail.com>
2020-06-17 13:19:44 +0530

    no changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,8 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  // useState() returns an array! 
-  // So, by using bracket as useState(0) we are using first value of array ([0]) 
-  // as value of our state and second ([1]) is the method we define to change our state.
-  
-  return (
-    <div className="App">
-        <h1>Understanding React Hooks</h1>
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Click to increase count</button>
-    </div>
-  );
+
+
 }
 
 export default App;
