2123fad537eb19a2210b75db313585653bc04615
raviteja8k <raviteja.tk@gmail.com>
2020-06-18 19:50:13 +0530

    UseEffect to fetch json data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
+import CountryList from './GetList';
 
-
-function App() {
-      const [fullName, setFullName] = useState({name: 'name', familyName: 'family'});
-      const [title] = useState('useEffect() in Hooks');
-      const[updateName] = useState(false);
-    
-    useEffect((updateName) => {
-        console.log('Running useEffect');
-        if(updateName)
-        setFullName({name: 'Updated name'});
-    }, []);
-    // Running useEffect without [] keeps running the loop like componentDidUpdate 
-    // To tell useEffect to act like componentDidMount just pass an empty array []
+function App() {  
    
-    //updateName({updateName: true});
-
     return(
         <div className="App">
-            <h1>Title: {title}</h1>
-            <h3>Name: {fullName.name}</h3>
-            <h3>Family Name: {fullName.familyName}</h3>
-            <button onClick={updateName}>Click to update</button>
-        </div>
+          <CountryList />
+         </div>
       );
  
 }
